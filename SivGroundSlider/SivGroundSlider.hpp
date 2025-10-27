99812c1d2ffb822ad3181c45a40398da720e3def
Ryoga-exe <git@ryoga.dev>
2025-10-28 06:25:14 +0900

    feat: add HWInfo/TouchFrame

--- SivGroundSlider/SivGroundSlider.hpp
+++ SivGroundSlider/SivGroundSlider.hpp
@@ -1,1 +1,24 @@
-﻿#pragma once
+﻿# pragma once
+# include <Siv3D.hpp>
+
+namespace SivGroundSlider
+{
+	struct HWInfo
+	{
+		String model;
+		String fwVersion;
+		String raw;
+		bool valid = false;
+	};
+
+	struct TouchFrame
+	{
+		std::array<uint8, 32> zones{};
+		uint64 timestampMS = 0;
+	};
+
+	class GroundSlider
+	{
+	public:
+	};
+}
