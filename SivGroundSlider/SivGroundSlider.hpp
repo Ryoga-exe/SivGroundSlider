5bbbf19187ae3ba62f724bfb8d22b125d8fce58d
Ryoga-exe <git@ryoga.dev>
2025-10-28 07:04:09 +0900

    feat: implement basic API

--- SivGroundSlider/SivGroundSlider.hpp
+++ SivGroundSlider/SivGroundSlider.hpp
@@ -20,5 +20,37 @@ namespace SivGroundSlider
 	class GroundSlider
 	{
 	public:
+		GroundSlider() = default;
+
+		~GroundSlider();
+
+		bool open(StringView port, int32 baudrate = 115200);
+
+		void close();
+
+		bool initialize(uint32 timeoutMS = 800);
+
+		bool startInput();
+
+		bool stopInput();
+
+		void update();
+
+		Optional<TouchFrame> pop();
+
+		// LEDs
+
+
+		bool isOpen() const;
+
+		bool initialized() const;
+
+		const HWInfo& hwInfo() const;
+
+	private:
+		Serial m_serial;
+		bool m_initialized = false;
 	};
 }
+
+# include "detail/GroundSlider.ipp"
---
+++ SivGroundSlider/detail/GroundSlider.ipp
@@ -0,0 +1,34 @@
+﻿# pragma once
+
+namespace SivGroundSlider
+{
+	GroundSlider::~GroundSlider()
+	{
+
+	}
+
+	bool GroundSlider::open(StringView port, int32 baudrate)
+	{
+		close();
+		return m_serial.open(port, baudrate);
+	}
+
+	void GroundSlider::close()
+	{
+		if (m_serial.isOpen())
+		{
+			m_serial.close();
+		}
+		m_initialized = false;
+	}
+
+	bool GroundSlider::isOpen() const
+	{
+		return m_serial.isOpen();
+	}
+
+	bool GroundSlider::initialized() const
+	{
+		return m_initialized;
+	}
+}
